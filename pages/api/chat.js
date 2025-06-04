acca9797af3e1b1fb2d0aed220bf2d1b1e66c9ed
hotomski <sofka.hrcak@gmail.com>
2025-06-04 12:37:49 +0200

    fixed answert when the limit is reached

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -44,10 +44,10 @@ export default async function handler(req, res) {
     // Initialize or increment the count
     userQuestionCounts[userKey] = userQuestionCounts[userKey] || 0;
   
-    if (userQuestionCounts[userKey] >= 2) {
+    if (userQuestionCounts[userKey] >= 1) {
       return res
         .status(429)
-        .json({ error: "Sorry folks, I'm not that rich. You reached your daily question limit ß(20 per day). You can ask more questions tomorrow!" });
+        .json({ answer: "Sorry folks, I'm not that rich. You reached your daily question limit ß(20 per day). You can ask more questions tomorrow!" });
     }
   
     userQuestionCounts[userKey]++;
