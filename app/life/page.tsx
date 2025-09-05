71fd03f55bf7a2842dda8c29a0100da2fcdef162
hotomski <sofka.hrcak@gmail.com>
2025-09-05 17:30:57 +0200

    Friends page added

--- app/life/page.tsx
+++ app/life/page.tsx
@@ -71,6 +71,16 @@ export default function LifePage() {
           </Link>
         </div>
 
+        {/* Gardening */}
+        <Link
+            href="/life/friends"
+            className="p-6 md:p-8 bg-white bg-opacity-90 rounded-lg shadow-lg border border-gray-200 w-full h-[180px] md:w-[450px] md:h-[350px] flex flex-col items-center justify-center transition-transform transform hover:scale-105"
+          >
+            <FaLeaf className="text-green-700 text-4xl md:text-5xl mb-4" />
+            <h1 className="text-xl md:text-2xl font-bold text-center">Friends</h1>
+          </Link>
+        </div>
+
         {/* Chatbot */}
         <Chatbot />
 
